b504fcdd3727cc7415eb85078d50d77709e6a55b
februar-y <jarmah241@gmail.com>
2021-01-13 16:13:27 -0500

    commit tree e596b24ef0b32564cfc353f27c178ac59bf180a8

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-function Filters() {
+function Filters({ onChangeType, onFindPetsClick }) {
+
+  // const handleChange = (e) => {
+  //   //console.log(e.target.value)
+  //   onChangeType(e.target.value)
+  // }
+
+  // const handleClick = () => {
+  //   onFindPetsClick
+  // }
+
   return (
     <div className="ui form">
       <h3>Animal type</h3>
       <div className="field">
-        <select name="type" id="type">
+        <select name="type" id="type" onChange={(e) => onChangeType(e.target.value)}>
           <option value="all">All</option>
           <option value="cat">Cats</option>
           <option value="dog">Dogs</option>
@@ -13,7 +23,7 @@ function Filters() {
         </select>
       </div>
 
-      <div className="field">
+      <div className="field" onClick={onFindPetsClick}>
         <button className="ui secondary button">Find pets</button>
       </div>
     </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,24 +1,30 @@
 import React from "react";
 
-function Pet() {
+function Pet( props ) {
+  const {id, type, gender, age, weight, name, onAdoptPet, isAdopted} = props
+
   return (
     <div className="card">
       <div className="content">
         <span className="header">
           {/*'♀' OR '♂' */}
-          PET NAME
+          <div>
+            {gender === "male" ? "♂" : "♀"}
+          </div>
+
+          {name}
         </span>
         <div className="meta">
-          <span className="date">PET TYPE</span>
+          <span className="date">{type}</span>
         </div>
         <div className="description">
-          <p>Age: PET AGE</p>
-          <p>Weight: PET WEIGHT</p>
+          <p>Age: {age}</p>
+          <p>Weight: {weight}</p>
         </div>
       </div>
       <div className="extra content">
-        <button className="ui disabled button">Already adopted</button>
-        <button className="ui primary button">Adopt pet</button>
+        <button className={!isAdopted ? "ui disabled button" : "ui primary button"}>Already adopted</button>
+        <button className={isAdopted ? "ui disabled button" : "ui primary button"} onClick={() => onAdoptPet(id)}>Adopt pet</button>
       </div>
     </div>
   );
