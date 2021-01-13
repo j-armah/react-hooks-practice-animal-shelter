861b9bfdbc79060ac12b39df78b898158885b731
februar-y <jarmah241@gmail.com>
2021-01-13 16:14:23 -0500

    completed all delivs, learn test failing though...

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,36 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+  const [isAdopted, setIsAdopted] = useState(false)
+
+  const handleOnChangeType = (filterSelect) => {
+    setFilters({...filters, type: filterSelect})
+  }
+
+  const handleOnFindPets = () => {
+    let query = filters.type
+
+    query === "all" ? query = "" : query = `?type=${query}`
+    fetch(`http://localhost:3001/pets${query}`)
+      .then(r => r.json())
+      .then(data => {
+        //console.log(data)
+        setPets(data)
+      })
+  }
+
+  const handleOnAdopt = (petId) => {
+    const adoptPet = pets.map(pet => {
+      if (pet.id === petId) {
+        pet.isAdopted = true
+        return pet
+      } else {
+        return pet
+      }
+    })
+    //console.log(adoptPet)
+    setPets(adoptPet)
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +45,16 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters 
+              onChangeType={handleOnChangeType} 
+              onFindPetsClick={handleOnFindPets}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser 
+              pets={pets}
+              onAdoptPet={handleOnAdopt}
+            />
           </div>
         </div>
       </div>
